e1f8bb4ba18ce3c40ee33b31efa1a055cf06aaa9
meg768 <magnus@egelberg.se>
2014-12-17 11:18:24 +0100

    -

--- software/run-test.cpp
+++ software/run-test.cpp
@@ -8,38 +8,25 @@ int main (int argc, char *argv[])
 {
 	Magick::InitializeMagick(*argv);
 
-	LogiMatrix matrix(32*3, 32*2);
+	LogiMatrix matrix();
 	
-	int red = 0;
-	int green = 0;
-	int blue = 0;
-	
-	int option = 0;
-	
-	while ((option = getopt(argc, argv, "r:g:b:")) != -1) {
-		switch (option) {
-			case 'r':
-				red = atoi(optarg);
-				break;
-			case 'g':
-				green = atoi(optarg);
-				break;
-			case 'b':
-				blue = atoi(optarg);
-				break;
-		}
-	}
 
-		for (int y = 0; y < 64; y++) {
-			
-			red = (15 * y	) / 64;
-			
-			for (int x = 0; x < 96; x++) {
-				matrix.setPixel(x, y, COLOR(red, green, blue));
-			}
-		}
-		
-		matrix.refresh();
+	Magick::Image imageA;
+	imageA.read("images/A.png");
+	
+	Magick::Image imageB;
+	imageB.read("images/B.png");
+	
+	Magick::Image imageC;
+	imageC.read("images/C.png");
+
+	imageA.getConstPixels(0, 0, 32, 32);
+	imageC.composite(imageA, 0, 0);
+	
+	
+	
+	matrix.drawImage(imageC);
+	matrix.refresh();
 		
 	
     return 0;
