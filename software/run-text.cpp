c53808e4e5ef57e52ef47714e3eff996a801caea
meg768 <magnus@egelberg.se>
2014-12-18 15:24:06 +0100

    -

--- software/run-text.cpp
+++ software/run-text.cpp
@@ -61,33 +61,56 @@ int main (int argc, char *argv[])
 			*/
 		
 		
-			Magick::Image tmp("32x32", "black");
-			
-			char fontFile[200];
-			sprintf(fontFile, "./fonts/%s.ttf", fontName);
-			
-			tmp.font(fontFile);
-			tmp.strokeColor("transparent");
-			tmp.fillColor(textColor);
-			tmp.fontPointsize(pointSize);
-			
-			Magick::TypeMetric metric;
-			tmp.fontTypeMetrics(text, &metric);
+		Magick::Image tmp("32x32", "black");
+		
+		char fontFile[200];
+		sprintf(fontFile, "./fonts/%s.ttf", fontName);
+		
+		tmp.font(fontFile);
+		tmp.strokeColor("transparent");
+		tmp.fillColor(textColor);
+		tmp.fontPointsize(pointSize);
+		
+		Magick::TypeMetric metric;
+		tmp.fontTypeMetrics(text, &metric);
+		
+		Magick::Image image(Magick::Geometry(metric.textWidth() + 2.0, 32.0), "black");
+		image.font(fontFile);
+		image.strokeColor("transparent");
+		image.fillColor(textColor);
+		image.fontPointsize(pointSize);
+		
+		image.draw(Magick::DrawableText(1, 16.0 + metric.textHeight() / 2.0 + metric.descent(), text));
 
-			Magick::Image image(Magick::Geometry(metric.textWidth() + 2.0, 32.0), "black");
-			image.font(fontFile);
-			image.strokeColor("transparent");
-			image.fillColor(textColor);
-			image.fontPointsize(pointSize);
 		
-			//image.resize(Magick::Geometry(metric.textWidth() + 2, 32));
-			image.draw(Magick::DrawableText(1, 16.0 + metric.textHeight() / 2.0 + metric.descent(), text));
-
 		
+		int screenHeight = matrix.height();
+		int screenWidth  = matrix.height();
+		int imageWidth   = image.columns();
+		int imageHeight  = image.rows();
+		int offsetX      = -screenWidth;
+		int offsetY      = -(screenHeight - imageHeight) / 2;
+		
+		int count = 0;
+		
+		
+		while (count < iterations) {
+			Magick::Image screen(Magick::Geometry(32, 32), "black");
 			
-		
-		//Magick::Image image;
-		//image.read(fileName);
+			screen.composite(image, offsetX, 0, Magick::CopyCompositeOp);
+			matrix.clear();
+			matrix.drawImage(image);
+			matrix.refresh();
+			
+			
+			if (++offsetX >= (imageWidth + screenWidth)) {
+				offsetX = -screenWidth;
+				count++;
+			}
+			
+			usleep(delay * 1000);
+		}
+/*
 		
 		int screenHeight = matrix.height();
 		int screenWidth  = matrix.height();
@@ -125,6 +148,7 @@ int main (int argc, char *argv[])
 
 			usleep(delay * 1000);
 		}
+ */
 	}
 	catch (std::exception &error) {
 		fprintf(stderr, "%s\n", error.what());
