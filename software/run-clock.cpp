abf8545058a5042582da21c506661f3aab389372
meg768 <magnus@egelberg.se>
2014-12-19 18:06:51 +0100

    -

--- software/run-clock.cpp
+++ software/run-clock.cpp
@@ -336,7 +336,7 @@ public:
 	
 	
 	void drawText(Magick::Image &image, double offsetX, double offsetY, char *text, int size, char *color) {
-		image.font("./fonts/Arial Black.ttf");
+		image.font("./fonts/OxygenMono.ttf");
 		image.strokeColor("transparent");
 		image.fillColor(color);
 		image.fontPointsize(size);
