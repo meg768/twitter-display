20f4545f1e654b63cb0102205572298ce9180df5
meg768 <magnus@egelberg.se>
2014-12-22 21:13:05 +0100

    -

--- software/run-clock.cpp
+++ software/run-clock.cpp
@@ -21,6 +21,65 @@ typedef struct {
 static rgb      hsv2rgb(hsv in);
 
 
+void HslToRgb(double h, double s, double v, double &r, double &g, double &b)
+{
+	double      hh, p, q, t, ff;
+	long        i;
+	rgb         out;
+	
+	if(s <= 0.0) {       // < is bogus, just shuts up warnings
+		r = v;
+		g = v;
+		b = v;
+		return;
+	}
+	hh = h;
+	if(hh >= 360.0) hh = 0.0;
+	hh /= 60.0;
+	i = (long)hh;
+	ff = hh - i;
+	p = in.v * (1.0 - s);
+	q = in.v * (1.0 - (s * ff));
+	t = in.v * (1.0 - (s * (1.0 - ff)));
+	
+	switch(i) {
+		case 0:
+			r = v;
+			g = t;
+			b = p;
+			break;
+		case 1:
+			r = q;
+			g = v;
+			b = p;
+			break;
+		case 2:
+			r = p;
+			g = v;
+			b = t;
+			break;
+			
+		case 3:
+			r = p;
+			g = q;
+			b = v;
+			break;
+		case 4:
+			r = t;
+			g = p;
+			b = v;
+			break;
+		case 5:
+		default:
+			r = v;
+			g = p;
+			b = q;
+			break;
+	}
+	return out;
+	
+}
+
 rgb hsv2rgb(hsv in)
 {
 	double      hh, p, q, t, ff;
@@ -132,70 +191,6 @@ static int hoursCoords[12][2] = {
 	{10,  7}
 	
 };
-
-void HSL2RGB(double h, double sl, double l, uint8_t &R, uint8_t &G, uint8_t &B)
-{
-	double v;
-	double r,g,b;
- 
-	r = l;   // default to gray
-	g = l;
-	b = l;
-	v = (l <= 0.5) ? (l * (1.0 + sl)) : (l + sl - l * sl);
-	if (v > 0)
-	{
-		double m;
-		double sv;
-		int sextant;
-		double fract, vsf, mid1, mid2;
-		
-		m = l + l - v;
-		sv = (v - m ) / v;
-		h *= 6.0;
-		sextant = (int)h;
-		fract = h - sextant;
-		vsf = v * sv * fract;
-		mid1 = m + vsf;
-		mid2 = v - vsf;
-		switch (sextant)
-		{
-			case 0:
-				r = v;
-				g = mid1;
-				b = m;
-				break;
-			case 1:
-				r = mid2;
-				g = v;
-				b = m;
-				break;
-			case 2:
-				r = m;
-				g = v;
-				b = mid1;
-				break;
-			case 3:
-				r = m;
-				g = mid2;
-				b = v;
-				break;
-			case 4:
-				r = mid1;
-				g = m;
-				b = v;
-				break;
-			case 5:
-				r = v;
-				g = m;
-				b = mid2;
-				break;
-		}
-	}
-	
-	R = (uint8_t)(r * 255.0f);
-	G = (uint8_t)(g * 255.0f);
-	B = (uint8_t)(b * 255.0f);
-}
 
 
 
