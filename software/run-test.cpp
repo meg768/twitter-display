e79e4652f1c82fce11c216201a62dbdce21e0071
meg768 <magnus@egelberg.se>
2014-12-22 16:53:35 +0100

    -

--- software/run-test.cpp
+++ software/run-test.cpp
@@ -246,7 +246,7 @@ public:
 		
 		hsv.h = hue;
 		hsv.s = 1.0;
-		hsv.v = 0.7;
+		hsv.v = 0.8;
 		
 		rgb = hsv2rgb(hsv);
 		
@@ -262,7 +262,7 @@ public:
 		
 		hsv.h = hue;
 		hsv.s = 1.0;
-		hsv.v = 0.25;
+		hsv.v = 0.3;
 		
 		rgb = hsv2rgb(hsv);
 		
