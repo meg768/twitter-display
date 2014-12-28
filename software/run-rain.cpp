4d1dbd7c00b1d07f16a4e5363f2169f007c3512c
meg768 <magnus@egelberg.se>
2014-12-28 23:27:51 +0100

    -

--- software/run-rain.cpp
+++ software/run-rain.cpp
@@ -324,8 +324,8 @@ int main (int argc, char *argv[])
 
 	MatrixAnimation animation;
 	animation.duration(60);
-	animation.speed(2.0);
-	animation.speed(120.0);
+	animation.speed(1.0);
+	animation.hue(120.0);
 	
 	int option = 0;
 	
