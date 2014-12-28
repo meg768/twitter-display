41552528cdad579babffddeabaeaa23352b71a63
meg768 <magnus@egelberg.se>
2014-12-28 23:17:39 +0100

    -

--- node/app.js
+++ node/app.js
@@ -58,7 +58,7 @@ function main() {
 		try {
 			var cmd = {};
 			cmd.command = './run-rain';
-			cmd.args = ['-d', '-1', '-s', '2.0' '-h', '120'];
+			cmd.args = ['-d', '-1'];
 			cmd.options = { cwd: "../software"};
 				
 			console.log('Starting animation: "%s"', cmd.command, cmd.args, cmd.options);	
