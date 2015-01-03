3b6987c6a3e5830c10d1df8c62198d44ccfe8608
meg768 <magnus@egelberg.se>
2015-01-03 15:17:53 +0100

    -

--- node/runqueue.js
+++ node/runqueue.js
@@ -1,54 +1,57 @@
 
 
-function Module() {
-	
-	var _this = this;
-
-	var _cmdQueue = [];
-	var _working = false;
-
-	function work() {
-		if (_cmdQueue.length > 0) {
-			
-			if (!_working) {
-				var item = _cmdQueue[0];
-
-				_working = true;
-
-				_this.emit('process', item, function() {
-					// Remove current job
-					_cmdQueue.shift();					
-					_working = false;
-					
-					// And continue with the next, when finished
-					work();
-					
-					
-				});
-			} 
-			
-		}
-		else {
-			_working = false;	
-			_this.emit('idle');
-		}
-	}
-	
-	_this.push = function(item) {
-		_cmdQueue.push(item);
-				
-		work();
-		
-	}
-};
-
-
 function main() {
-	module.exports = Module;
-	
+
 	var util   = require('util');
 	var events = require('events');
+
+	function Module() {
+		
+		var _this = this;
+	
+		var _queue = [];
+		var _working = false;
+	
+		function work() {
+			if (_queue.length > 0) {
+				
+				if (!_working) {
+					var item = _queue[0];
+	
+					_working = true;
+	
+					_this.emit('process', item, function() {
+						// Remove current job
+						_queue.shift();					
+						_working = false;
+						
+						// And continue with the next, when finished
+						work();
+						
+						
+					});
+				} 
+				
+			}
+			else {
+				_working = false;	
+				_this.emit('idle');
+			}
+		}
+		
+		_this.push = function(item) {
+			_queue.push(item);
+					
+			work();
+			
+		}
+	};
+
+	
 	util.inherits(Module, events.EventEmitter);
+
+	module.exports = Module;
+
 }
 
 
